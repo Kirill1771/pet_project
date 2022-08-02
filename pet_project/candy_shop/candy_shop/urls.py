64576d8d619794c0fc5b1bcbbb99e44f6e242e9a
Kirill1771 <klipakirill30@gmail.com>
2022-08-02 17:55:51 +0300

    add some templates, try to fix urls

--- pet_project/candy_shop/candy_shop/urls.py
+++ pet_project/candy_shop/candy_shop/urls.py
@@ -1,10 +1,10 @@
-from django.urls import include, re_path
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^', include('candy_shop_app.urls'), name='candy_shop'),
-    re_path(r'^cart/', include('mycart.urls'), name='cart'),
-    re_path(r'^orders/', include('orders.urls'), name='orders'),
-    re_path(r'^account/', include('account.urls')),
+    path(r'^admin/', admin.site.urls),
+    path(r'^', include('candy_shop_app.urls'), name='candy_shop'),
+    path(r'^cart/', include('mycart.urls'), name='cart'),
+    path(r'^orders/', include('orders.urls'), name='orders'),
+    path(r'^account/', include('account.urls')),
 ]
