d14c20bfac62db9b059d70342751dce1e2b82bf9
Kirill1771 <klipakirill30@gmail.com>
2022-09-15 14:06:29 +0300

    modified urls

--- cart/urls.py
+++ cart/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     re_path(r'^add/(?P<product_id>[0-9]+)/$', AddToCartView.as_view(), name='add'),
     re_path(r'^remove/(?P<product_id>[0-9]+)/$', RemoveCartItemView.as_view(), name='remove'),
     re_path(r'^update/(?P<pk>[0-9]+)/$', UpdateCartItemView.as_view(), name='update'),
-    path('api/cart/', CartDetailView.as_view(), name='cart'),
-    re_path(r'api/cart/(?P<id>[0-9]+)/$', CartUpdateDeleteView.as_view(), name='update_cart'),
+    path('api/cart/', CartDetailView.as_view(), name='api_cart'),
+    re_path(r'api/cart/(?P<id>[0-9]+)/$', CartUpdateDeleteView.as_view(), name='update_cart_api'),
 ]
